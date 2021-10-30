1a892be43e5ac8f0b2c7e3b42c9a46e817cf826a
Jonas Nunes <70301882+jonasnunes@users.noreply.github.com>
2021-10-30 01:56:46 -0300

    Add files via upload
    Novos estilos, novas marcações html e adicionando javascript nessa segunda aula da maratona

---
+++ dev-finances/scripts/script.js
@@ -0,0 +1,12 @@
+const modal = {
+  open(){
+    // Abrir modal
+    // Adicionar a classe active ao modal
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close(){
+    // Fechar modal
+    // Remover a classe active do modal
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
